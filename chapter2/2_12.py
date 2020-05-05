9d105cffc7c37e6d3e3b94a14badfc8bcebc912e
thollang <thollang@hotmail.com>
2020-05-05 16:39:45 +0800

    update chapter2

---
+++ chapter2/2_12.py
@@ -0,0 +1,12 @@
+def main():
+    for i in range(1,11):
+        print(i, end=' ')
+    print('\n')
+    
+    i=1
+    while i<=10:
+        print(i, end=' ')
+        i += 1
+        
+if __name__ == '__main__':
+    main()
